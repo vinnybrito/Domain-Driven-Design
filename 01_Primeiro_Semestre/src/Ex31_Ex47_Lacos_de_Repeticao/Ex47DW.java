6435b336ff9146af74051c36f4174590119a8ac3
vinnybrito <viny.rbrito15@hotmail.com>
2024-07-20 17:00:38 -0300

    Atualizando Repositório

--- 01_Primeiro_Semestre/src/Ex31_Ex47_Lacos_de_Repeticao/Ex47DW.java
+++ 01_Primeiro_Semestre/src/Ex31_Ex47_Lacos_de_Repeticao/Ex47DW.java
@@ -1,5 +1,7 @@
 package Ex31_Ex47_Lacos_de_Repeticao;
 
+import java.util.Scanner;
+
 public class Ex47DW {
 	
 	// 47 - Crie um programa que solicite ao usuário que digite um número positivo. 
@@ -9,8 +11,45 @@ public class Ex47DW {
 	// novamente. Caso a resposta seja inválida, solicite que o usuário digite novamente.
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		Scanner scanner = new Scanner(System.in);
 
+        String resposta = "S";
+        int i;
+
+        while (resposta.toUpperCase().equals("S")) {	
+        	int fatorial = 1;
+        	
+            System.out.print("\nDigite o número (positivo) que você quer calcular o fatorial: ");
+            int x = scanner.nextInt();
+
+            while (x < 0) {
+                System.out.println("O valor deve ser positivo. Digite novamente: ");
+                x = scanner.nextInt();
+            }
+            
+            if (x == 0) {
+                System.out.println("0! = 1");
+            } else {
+            	i = x;
+            	do {
+            		fatorial = fatorial * i;
+            		i--;
+            	} while (i > 0);
+                System.out.println(x + "! = " + fatorial);
+            }
+
+            System.out.print("\nDeseja calcular novamente?\n(Sim = S / Não = N): ");
+            resposta = scanner.next();
+
+            while ((!resposta.toUpperCase().equals("S")) && (!resposta.toUpperCase().equals("N"))) {
+                System.out.print("\nFavor responder com S para SIM ou N para NÃO: ");
+                resposta = scanner.next();
+            }
+        }
+
+        System.out.println("\nFim do programa!");
+        
+        scanner.close();
 	}
 
 }
--- 01_Primeiro_Semestre/src/Ex31_Ex47_Lacos_de_Repeticao/Ex47F.java
+++ 01_Primeiro_Semestre/src/Ex31_Ex47_Lacos_de_Repeticao/Ex47F.java
@@ -1,5 +1,7 @@
 package Ex31_Ex47_Lacos_de_Repeticao;
 
+import java.util.Scanner;
+
 public class Ex47F {
 	
 	// 47 - Crie um programa que solicite ao usuário que digite um número positivo. 
@@ -9,8 +11,42 @@ public class Ex47F {
 	// novamente. Caso a resposta seja inválida, solicite que o usuário digite novamente.
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		Scanner scanner = new Scanner(System.in);
 
+        String resposta = "S";
+
+        while (resposta.toUpperCase().equals("S")) {	
+        	int fatorial = 1;
+        	
+            System.out.print("\nDigite o número (positivo) que você quer calcular o fatorial: ");
+            int x = scanner.nextInt();
+
+            while (x < 0) {
+                System.out.println("O valor deve ser positivo. Digite novamente: ");
+                x = scanner.nextInt();
+            }
+            
+            if (x == 0) {
+                System.out.println("0! = 1");
+            } else {
+                for (int i = x; i > 0; i--) {
+                    fatorial = fatorial * i;
+                }
+                System.out.println(x + "! = " + fatorial);
+            }
+
+            System.out.println("\nDeseja calcular novamente?\n(Sim = S / Não = N): ");
+            resposta = scanner.next();
+
+            while ((!resposta.toUpperCase().equals("S")) && (!resposta.toUpperCase().equals("N"))) {
+                System.out.print("\nFavor responder com S para SIM ou N para NÃO: ");
+                resposta = scanner.next();
+            }
+        }
+
+        System.out.println("\nFim do programa!");
+        
+        scanner.close();
 	}
 
 }
